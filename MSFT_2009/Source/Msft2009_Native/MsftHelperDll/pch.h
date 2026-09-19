6e7c04304f3d61b34abb9417f022b24a19d74d00
Dark Monkey <295954973+v8k6b2jjps-prog@users.noreply.github.com>
2026-09-19 15:51:50 +0300

    Add files via upload

---
+++ MSFT_2009/Source/Msft2009_Native/MsftHelperDll/pch.h
@@ -0,0 +1,18 @@
+#define NOMINMAX
+
+#ifndef PCH_H
+#define PCH_H
+
+#include <windows.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <stdexcept>
+#include <cstring>
+#include <cstdint>
+#include "framework.h"
+
+#endif // PCH_H
+
+// add headers that you want to pre-compile here
